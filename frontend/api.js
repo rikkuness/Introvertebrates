121238f92acb53628ee7b15d47950c50a5ac6fc9
Darrian <rikkuness@gmail.com>
2017-04-29 21:43:20 +0100

    Added user authentication
    Fixes #20

--- frontend/api.js
+++ frontend/api.js
@@ -1,9 +1,7 @@
 const router = require('express').Router()
 
 router.get('/user', (req, res) => res.json({
-  name: req.user.displayName,
-  ign: req.user._json.user_metadata.ign,
-  admin: req.user._json.app_metadata.admin
+  name: req.user.displayName
 }))
 
 module.exports = router
