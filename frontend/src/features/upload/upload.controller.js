4f225740c3acaf6ff492e311042424498da44a45
Steven Frazier-Roberts <steven@stevens-mbp.home>
2017-05-03 18:54:48 +0100

    angry pug

--- frontend/src/features/upload/upload.controller.js
+++ frontend/src/features/upload/upload.controller.js
@@ -5,36 +5,72 @@ export default class UploadController {
 
     console.log($scope)
     var self = $scope
-    var button_ng = angular.element(document).ready(function(){
+    var button_ng = angular.element(document).ready(function () {
 
       if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position){
+        navigator.geolocation.getCurrentPosition(function (position) {
           console.log("heleo")
           console.log(self)
-          $scope.$apply(function(){
+          $scope.$apply(function () {
             self.lat = position.coords.latitude
             self.long = position.coords.longitude
-            self.loading=false
-            
+            self.loading = false
+
           })
 
-        }, function(err) {
+        }, function (err) {
           console.log("Geolocation failed")
-          $scope.$apply(function(){
-          self.loading=false
-          //$scope.loadingError = "Failed to get device geolocation"
+          $scope.$apply(function () {
+            self.loading = false
+            //$scope.loadingError = "Failed to get device geolocation"
 
-            })
+          })
         })
       } else {
-         $scope.$apply(function(){
-          self.loading=false
+        $scope.$apply(function () {
+          self.loading = false
 
-            })
+        })
         console.log("Geolocation is not supported by this browser.")
 
       }
     })
-  
+
+    $scope.onChange = function picChange(evt) {
+      //bring selected photo in
+      //get files captured through input
+      var fileInput = evt.target.files;
+
+      if (fileInput.length > 0) {
+        //get the file
+      }
+
+      //window url 
+      var windowURL = $scope.window.URL || $scope.window.webkitURL;
+
+      //picture url
+      var picURL = windowURL.createObjectURL(fileInput[0]);
+
+      //get canvas
+      var photoCanvas = angular.element("capturedPhoto");
+
+      var ctx = photoCanvas.getContext("2d");
+
+      //create image
+      var photo = new Image();
+
+      photo.onload = function () {
+        //draw photo into canvas when ready
+        ctx.drawImage(photo, 0, 0, 500, 400);
+      };
+
+      //load photo into canvas
+      photo.src = picURL;
+      //release object url
+      windowURL.revokeObjectURL(picURL);
+
+
+    }
+
   }
 }
